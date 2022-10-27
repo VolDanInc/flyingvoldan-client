926db7bce864bb3a4b9f886f4f6477778f7aa17a
VKhodyrev <vkhodyrev1@gmail.com>
2022-10-27 18:44:53 +0300

    return back app after solving conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+// src/App.js
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import SignupPage from "./pages/SignupPage";  // <== IMPORT
+import LoginPage from "./pages/LoginPage";  // <== IMPORT
+import TripsPage from "./pages/TripsPage";  // <== IMPORT
+import Navbar from "./components/NavBar"; 
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar />
+ 
+      <Routes>      
+     {/*   <Route path="/" element={ <HomePage /> } />
+        
+        <Route path="/projects/:projectId" element={ <ProjectDetailsPage /> } />
+        <Route path="/projects/edit/:projectId" element={ <EditProjectPage /> } />*/}
+        <Route path="/trips" element={ <TripsPage /> } />
+        <Route path="/signup" element={ <SignupPage /> } />
+        <Route path="/login" element={ <LoginPage /> } />
+      </Routes>
     </div>
   );
 }
