514ccc1dc63241a7a2627eef0abb82de4354b5ad
VKhodyrev <vkhodyrev1@gmail.com>
2022-11-01 10:39:07 +0200

    applying bootstrap cards

--- src/pages/CommentsPage.js
+++ src/pages/CommentsPage.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 //import { Link, useNavigate } from "react-router-dom";
 //import { AuthContext } from '../context/auth.context';
 
@@ -40,14 +42,36 @@ function CommentsPage() {
             {
                 tripsArr.map((trip, index) => {
                     return (
-                        <div className="Comments" key={index}>
-                            <h3>Created by: {trip.userId.name}</h3>
-                            <p>Aircraft: {trip.aircraftId.name}</p>
-                            <p>Comment: {trip.review}</p>
-                            <p>Rating: {trip.reviewStars}</p>
-                            <p>Created: {trip.updatedAt}</p>
-                            <hr />
-                        </div>
+                        // <div className="Comments" key={index}>
+                        //     <h3>Created by: {trip.userId.name}</h3>
+                        //     <p>Aircraft: {trip.aircraftId.name}</p>
+                        //     <p>Comment: {trip.review}</p>
+                        //     <p>Rating: {trip.reviewStars}</p>
+                        //     <p>Created: {trip.updatedAt}</p>
+                        //     <hr />
+                        // </div>
+                        <Card
+                            bg='dark'
+                            key='Dark'
+                            text='white'
+                            // style={{ width: '18rem' }}
+                            className="mb-2"
+                        >
+                            <Card.Header>Created by: {trip.userId.name}</Card.Header>
+                            <Card.Body>
+                                <Card.Title>Aircraft: {trip.aircraftId.name}</Card.Title>
+                                <Card.Text>
+                                    Comment: {trip.review}
+                                </Card.Text>
+                                <Button variant="outline-secondary">Like</Button>
+                                <Button variant="outline-secondary">Unlike</Button>
+                            </Card.Body>
+                            <Card.Footer className="text-muted">
+                                <p>Rating: {trip.reviewStars}</p>
+                                <p>Created: {trip.updatedAt}</p>
+                            </Card.Footer>
+                        </Card>
+
                     );
                 })
             }
