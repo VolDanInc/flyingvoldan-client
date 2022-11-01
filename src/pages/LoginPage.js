136c2b0cba0eb6ab472f8d9a0488b48fed04144a
VKhodyrev <vkhodyrev1@gmail.com>
2022-11-01 20:53:15 +0200

    css login start imlementation

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,33 +1,34 @@
 // src/pages/LoginPage.js
- 
+
 import { useState, useContext } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from '../context/auth.context';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 
- 
- 
 function LoginPage(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
-  
+
   const navigate = useNavigate();
   const { storeToken, authenticateUser } = useContext(AuthContext);
- 
+
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
- 
-  
+
+
   const handleLoginSubmit = (e) => {
     e.preventDefault();
     const requestBody = { email, password };
- 
+
     axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, requestBody)
       .then((response) => {
-      // Request to the server's endpoint `/auth/login` returns a response
-      // with the JWT string ->  response.data.authToken
+        // Request to the server's endpoint `/auth/login` returns a response
+        // with the JWT string ->  response.data.authToken
         //console.log('JWT token', response.data.authToken );
 
         storeToken(response.data.authToken);
@@ -40,10 +41,10 @@ function LoginPage(props) {
         setErrorMessage(errorDescription);
       })
   };
-  
+
   return (
     <div className="LoginPage">
-      <h1>Login</h1>
+      {/* <h1>Login</h1>
  
       <form onSubmit={handleLoginSubmit}>
         <label>Email:</label>
@@ -66,10 +67,38 @@ function LoginPage(props) {
       </form>
       { errorMessage && <p className="error-message">{errorMessage}</p> }
  
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
+      <p>Don't have an account yet?</p> 
+      <Link to={"/signup"}> Sign Up</Link>*/}
+
+      <Form
+        style={{ width: '60vw', backgroundColor: "#393838", padding: "20px", color: "white", borderRadius: "10px"}}
+        onSubmit={handleLoginSubmit}
+      >
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Email address"
+          className="mb-3"
+        >
+          <Form.Control type="email" value={email} onChange={handleEmail} />
+        </FloatingLabel>
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+
+        <FloatingLabel controlId="floatingPassword" label="Password">
+          <Form.Control type="password" value={password} onChange={handlePassword} />
+        </FloatingLabel>
+
+        <Button variant="outline-secondary" type="submit" >
+          Login
+        </Button>
+        <p>Don't have an account yet?</p>
+        <Link className="btn btn-outline-secondary" to={"/signup"} role="button"> Sign Up </Link>
+      </Form>
+
     </div>
   )
 }
- 
+
 export default LoginPage;
