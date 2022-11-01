20ae062643bd3c45cf71398e4b52ea76bad74767
VKhodyrev <vkhodyrev1@gmail.com>
2022-11-01 13:51:23 +0200

    sync to github

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -40,19 +40,19 @@ function HomePage() {
         <div id="App" >
             {/* <Navbar pageWrapId={"page-wrap"} outerContainerId={"App"} />
             <div id="page-wrap"> */}
-               
-                    {/* <div className='ratio ratio-16x9'>
+
+            {/* <div className='ratio ratio-16x9'>
             <video src=".public/AircraftsImages/video.mp4" type="video/mp4" id="background-video" loop autoPlay allowFullScreen/>
             </div> */}
-            
-  
+
+
 
 
             <h1>Choose aircraft</h1>
 
-           
-            
-        {/* {
+
+
+            {/* {
             aircrafts.map((aircraft, index) => {
                 return (
                     <div className="card" key={index}>
@@ -85,7 +85,7 @@ function HomePage() {
            
         // </div>    */}
 
-           
+
             {
                 aircrafts.map((aircraft, index) => {
                     return (
@@ -109,7 +109,13 @@ function HomePage() {
                         //             <Link to={`/trips/create/${aircraft._id}`}>Book trip</Link>)
                         //         : <></>}
                         // </div>
-                        <Card style={{ width: '18rem' }}>
+                        <Card
+                            bg='dark'
+                            key='Dark'
+                            text='white'
+                            style={{ width: '18rem' }}
+                            className="mb-2"
+                        >
                             <Card.Img variant="top" src={aircraft.img} onError={({ currentTarget }) => {
                                 currentTarget.onerror = null;
                                 currentTarget.src = "https://www.pngitem.com/pimgs/m/119-1197957_lear-jet-clip-arts-liar-jet-icon-png.png";
@@ -122,7 +128,7 @@ function HomePage() {
                                 <Link class="btn btn-outline-secondary" to={`/aircrafts/${aircraft._id}`} role="button"> Details</Link>
                                 {user
                                     ? (user.isAdmin ? <Link class="btn outline-secondary" to={`/aircrafts/edit/${aircraft._id}`} role="button">Edit</Link> :
-                                    <Link class="btn btn-outline-secondary" to={`/trips/create/${aircraft._id}`} role="button">Book trip</Link>)
+                                        <Link class="btn btn-outline-secondary" to={`/trips/create/${aircraft._id}`} role="button">Book trip</Link>)
                                     : <></>}
                             </Card.Body>
                         </Card>
