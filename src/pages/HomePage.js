2679f5e4c990fc3c3732afb2f6e2a625c02ebb13
Daniel Plesa <danielplesa@Daniels-MacBook-Pro.local>
2022-11-02 10:38:11 +0100

    update header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -47,7 +47,7 @@ function HomePage() {
             <source src={video} type="video/mp4" />
            </video>
            <div className='content'>
-            <h1 id="headline" >THE SKY IS NOT THE LIMIT.</h1>
+            <h1 id="headline" >THE SKY IS NOT THE LIMIT !!</h1>
             <p id="2nd-headline">EXPERIENCE FLYING LIKE NEVER BEFORE.</p>
             <p></p>
             <Button type="button" class="btn btn-outline-warning">EXPLORE</Button>
